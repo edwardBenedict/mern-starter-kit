f486e05b79309da92840fd996548ed0761d0717d
edwardBenedict <edwardbenedict92@gmail.com>
2022-01-06 22:25:01 +0100

    feat: add comments

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 
 const app = express();
 
+// Connect to MongoDB
 const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.amewz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log("dbURI", dbURI);
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -18,12 +18,15 @@ mongoose
 
 const PORT = process.env.SERVER_PORT || 5000;
 
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, "build")));
 
+// Put all API endpoints under '/api'
 app.get("/api/", (req, res) => {
   res.send({ message: "Hello World" });
 });
 
+// Listen to requests on port 5000
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
   app.get("*", (req, res) => {
